47b796c3c18ef98ec35860438c37a5f511e6a939
KulsoomKamilKhan <71809252+KulsoomKamilKhan@users.noreply.github.com>
2020-10-29 23:22:22 +0400

    game play

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 function openWindow()
  {
-    newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");   
+    newWindow = window.open("", null, "height=300,width=500,status=yes,toolbar=no,menubar=no,location=no");   
 }
