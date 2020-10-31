32a7108b0cb93de57343663abb9c031889cdf2fd
SaiShriyaSurla <65212816+SaiShriyaSurla@users.noreply.github.com>
2020-11-01 02:47:31 +0400

    connect4

--- connect4.js
+++ connect4.js
@@ -1,153 +1,150 @@
-(function() {
+while (!player1){
+    var player1 = prompt('Player One: Enter your name. You will be red.');
+};
+var player1Color = 'red';
 
-	var ConnectFour = function() {
+while (!player2){
+    var player2 = prompt('Player Two: Enter your name. You will be yellow.');
+};
+var player2Color = 'yellow';
 
-		gameBoard = {};
-		currentPlayer = 'red';
-		numRows = 6;
-		numCols = 7;
-		numTurns = 0;
-		
-		_init = function() {
-			
-			var columns;
-			
-			columns = document.querySelectorAll('.column');
-			
-			Array.prototype.forEach.call(columns, function(col) {
-				col.addEventListener('click', function() {
-					markNextFree(col.getAttribute('data-x'));
-				});
-			});
-			
-			for(var x = 0; x <= numRows; x++) {
-			
-				gameBoard[x] = {};
-				
-				for(var y = 0; y <= numCols; y++) {
-					gameBoard[x][y] = 'free';
-				}
-			}
-			
-		};
-		
-		var markNextFree = function(x) {
+// Selectors
 
-			var nextY;
-			
-			nextY = false;
-			
-			for(var y = 0; y < numRows; y++) {
-				if(gameBoard[x][y] === 'free') {
-					nextY = y;
-					break;
-				}
-			}
-			
-			if(nextY === false) {
-				alert('No free spaces in this column. Try another.');
-				return false;
-			}
-			
-			gameBoard[x][nextY] = currentPlayer;
-			
-			document.querySelector('#column-'+x+' .row-'+nextY+' circle').setAttribute(
-					'class', currentPlayer
-			);
-			
-			if(isWinner(parseInt(x), nextY)) {
-				alert(currentPlayer+' wins!');
-				clearBoard();
-				return true;
-			}
 
-			numTurns = numTurns + 1;
+var tableRow = document.getElementsByTagName('tr');
+var tableData = document.getElementsByTagName('td');
+var playerTurn = document.querySelector('.player-turn');
+const slots = document.querySelectorAll('.slot');
+const resetBtn = document.querySelector('.reset');
 
-			if(numTurns >= numRows * numCols) {
-				alert('It\'s a tie!');
-				clearBoard();
-				return true;				
-			}
 
-			currentPlayer = currentPlayer === 'red' ? 'yellow' : 'red';
+var currentPlayer = 1;
+let winner;
+playerTurn.textContent = `${player1}'s turn!`
 
-		};
-		
-		var clearBoard = function() {
-			
-			Array.prototype.forEach.call(document.querySelectorAll('circle'), function(piece) {
-				piece.setAttribute('class', 'free');
-			});
-			
-			gameBoard = {};
+// Log cell coordinates when clicked
 
-			for(var x = 0; x <= numRows; x++) {
-			
-				gameBoard[x] = {};
-				
-				for(var y = 0; y <= numCols; y++) {
-					gameBoard[x][y] = 'free';
-				}
-        
-          console.log(gameBoard);
-			}
+for (i = 0; i < tableData.length; i ++){
+    tableData[i].addEventListener('click', (e) =>{
+        console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
 
-			numTurns = 0;
-			
-			return gameBoard;
 
-		};
+// Funtions
 
-		var isWinner = function(currentX, currentY) {
-			return checkDirection(currentX, currentY, 'vertical') || 
-				checkDirection(currentX, currentY, 'diagonal') || 
-				checkDirection(currentX, currentY, 'horizontal');
-		};
-		
-		var isBounds = function(x, y) {
-			return (gameBoard.hasOwnProperty(x) && typeof gameBoard[x][y] !== 'undefined');
-		};
+function changeColor(e){
+    // Get clicked column index
+    let column = e.target.cellIndex;
+    let row = [];
 
-		var checkDirection = function(currentX, currentY, direction) {
-		
-			var chainLength, directions;
-			
-			directions = {
-				horizontal: [
-					[0, -1], [0, 1]
-				],
-				vertical: [
-					[-1, 0], [1, 0]
-				],
-				diagonal: [
-					[-1, -1], [1, 1], [-1, 1], [1, -1]
-				]
-			};
-			
-			chainLength = 1;
-			
-			directions[direction].forEach(function(coords) {
-				
-				var i = 1;
+    for (i = 5; i > -1; i--){
+        if (tableRow[i].children[column].style.backgroundColor == 'white'){
+            row.push(tableRow[i].children[column]);
+            if (currentPlayer === 1){
+                row[0].style.backgroundColor = 'red';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player1} WINS!!`;
+                    playerTurn.style.color = player1Color;
+                    return alert(`${player1} WINS!!`);
+                }else if (drawCheck()){
+                    playerTurn.textContent = 'DRAW!';
+                    return alert('DRAW!');
+                }else{
+                    playerTurn.textContent = `${player2}'s turn`
+                    return currentPlayer = 2;
+                }
+            }else{
+                row[0].style.backgroundColor = 'yellow';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player2} WINS!!`;
+                    playerTurn.style.color = player2Color;
+                    return alert(`${player2} WINS!!`);
+                }else if (drawCheck()){
+                    playerTurn.textContent = 'DRAW!';
+                    return alert('DRAW!');
+                }else{
+                    playerTurn.textContent = `${player1}'s turn`;
+                    return currentPlayer = 1;
+                }
+                
+            }
+        }
+    }
+   
+}
 
-				while( isBounds(currentX + (coords[0] * i), currentY + (coords[1] * i)) && 
-					(gameBoard[currentX + (coords[0] * i)][currentY + (coords[1] * i)] === currentPlayer)
-				) {
-					chainLength = chainLength + 1; 
-					i = i + 1; 
-				};
-				
-			});
-			
-			return (chainLength >= 4);
-			
-		};
-		
-		_init();
-		
-	};
+Array.prototype.forEach.call(tableData, (cell) => {
+    cell.addEventListener('click', changeColor);
+    // Set all slots to white for new game.
+    cell.style.backgroundColor = 'white';
+});
 
-	ConnectFour();
-  
-	
-})();
+function colorMatchCheck(one, two, three, four){
+    return (one === two && one === three && one === four && one !== 'white' && one !== undefined);
+}
+
+function horizontalCheck(){
+    for (let row = 0; row < tableRow.length; row++){
+        for (let col =0; col < 4; col++){
+           if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor,tableRow[row].children[col+1].style.backgroundColor, 
+                                tableRow[row].children[col+2].style.backgroundColor, tableRow[row].children[col+3].style.backgroundColor)){
+               return true;
+           }
+        }
+    }
+}
+
+function verticalCheck(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col].style.backgroundColor,
+                                tableRow[row+2].children[col].style.backgroundColor,tableRow[row+3].children[col].style.backgroundColor)){
+                return true;
+            };
+        }   
+    }
+}
+
+function diagonalCheck(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col+1].style.backgroundColor,
+                tableRow[row+2].children[col+2].style.backgroundColor,tableRow[row+3].children[col+3].style.backgroundColor)){
+                    return true;
+                }
+            }
+        }
+
+}
+
+function diagonalCheck2(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 5; row > 2; row--){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row-1].children[col+1].style.backgroundColor,
+                tableRow[row-2].children[col+2].style.backgroundColor,tableRow[row-3].children[col+3].style.backgroundColor)){
+                    return true;
+            }
+        }
+    }
+}
+
+function drawCheck(){
+    let fullSlot = []
+    for (i=0; i < tableData.length; i++){
+        if (tableData[i].style.backgroundColor !== 'white'){
+            fullSlot.push(tableData[i]);
+        }
+    }
+    if (fullSlot.length === tableData.length){
+        return true;
+    }
+}
+
+resetBtn.addEventListener('click', () => {
+    slots.forEach(slot => {
+        slot.style.backgroundColor = 'white';
+    });
+    playerTurn.style.color = 'black';
+    return (currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn` : playerTurn.textContent = `${player2}'s turn`);
+});
