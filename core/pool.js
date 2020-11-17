12b8b5e6dc6a82b2a54c01891e57ce0949a2138e
sm-360 <60068853+sm-360@users.noreply.github.com>
2020-11-18 03:05:18 +0400

    signup and login pages created

---
+++ core/pool.js
@@ -0,0 +1,33 @@
+const util = require('util');
+const mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var server = require('http').createServer(app);
+const pageRouter = require('./routes/pages');
+var io = require('socket.io')(server);
+
+/**
+ * Connection to the database.
+ *  */
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'Your database username', // use your mysql username.
+    password: 'Your database password', // user your mysql password.
+    database: 'logindata'
+});
+
+pool.getConnection((err, connection) => {
+    if(err) 
+        console.error("Something went wrong connecting to the database ...");
+    
+    if(connection)
+        connection.release();
+    return;
+});
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
