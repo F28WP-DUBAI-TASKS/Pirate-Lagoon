c079ae895221f47e595a9c0edb6301cd2515e7f9
sm-360 <60068853+sm-360@users.noreply.github.com>
2020-11-26 10:22:31 +0400

    heroku link

---
+++ core/dist/pool.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var util = require('util');
+
+var mysql = require('mysql');
+/**
+ * Connection to the database.
+ *  */
+
+
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  // use your mysql username.
+  password: '123abc',
+  // user your mysql password.
+  database: 'www'
+});
+pool.getConnection(function (err, connection) {
+  if (err) console.log(err); // console.error("Something went wrong connecting to the database ...");
+
+  if (connection) connection.release();
+  return;
+});
+pool.query = util.promisify(pool.query);
+module.exports = pool;
---
+++ core/dist/user.dev.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var pool = require('./pool');
+
+var bcrypt = require('bcrypt');
+
+function User() {}
+
+;
+User.prototype = {
+  // Find the user data by id or username.
+  find: function find() {
+    var user = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+    var callback = arguments.length > 1 ? arguments[1] : undefined;
+
+    // if the user variable is defind
+    if (user) {
+      // if user = number return field = id, if user = string return field = username.
+      var field = Number.isInteger(user) ? 'id' : 'username';
+    } // prepare the sql query
+
+
+    var sql = "SELECT * FROM users WHERE ".concat(field, " = ?");
+    pool.query(sql, user, function (err, result) {
+      if (err) throw err;
+
+      if (result.length) {
+        callback(result[0]);
+      } else {
+        callback(null);
+      }
+    });
+  },
+  // This function will insert data into the database. (create a new user)
+  // body is an object 
+  create: function create(body, callback) {
+    var pwd = body.password; // Hash the password before insert it into the database.
+
+    body.password = bcrypt.hashSync(pwd, 10); // this array will contain the values of the fields.
+
+    var bind = []; // loop in the attributes of the object and push the values into the bind array.
+
+    for (var prop in body) {
+      bind.push(body[prop]);
+    } // prepare the sql query
+
+
+    var sql = "INSERT INTO users(username, fullname, password) VALUES (?, ?, ?)"; // call the query give it the sql string and the values (bind array)
+
+    pool.query(sql, bind, function (err, result) {
+      if (err) throw err; // return the last inserted id. if there is no error
+
+      callback(result.insertId);
+    });
+  },
+  login: function login(username, password, callback) {
+    // find the user data by his username.
+    this.find(username, function (user) {
+      // if there is a user by this username.
+      if (user) {
+        // now we check his password.
+        if (bcrypt.compareSync(password, user.password)) {
+          // return his data.
+          callback(user);
+          return;
+        }
+      } // if the username/password is wrong then return null.
+
+
+      callback(null);
+    });
+  }
+};
+module.exports = User;
